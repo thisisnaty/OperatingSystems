0d2cce47d14a39ad29807c7247ca546decec667c
thisisnaty <garcia.doe@gmail.com>
2015-11-30 13:06:28 -0600

    Created method isLoaded and load

--- ProyectoFinalOperativos_Equipo4/src/Classes/EventHandler.java
+++ ProyectoFinalOperativos_Equipo4/src/Classes/EventHandler.java
@@ -29,4 +29,74 @@ public class EventHandler {
             secondaryMemory[i] = new Frame();
         }
     }
+    
+    public void load(Process p, Summary report) {
+        boolean fitsInMainMemory = false;
+        int frameAvailability = 0; 
+        List<Integer> mainMemoryFrameAvailability = new ArrayList<Integer>();
+        List<Integer> secondaryMemoryFrameAvailability = new ArrayList<Integer>();
+        
+        if (isLoaded(p)) {
+            System.out.println("Este proceso ya está cargado en memoria");
+        }
+        else {
+            //cargarlo en memoria
+            for (int i = 0; i < 256 && frameAvailability != p.getPageNumber(); i++) {
+                if (mainMemory[i].getProcessID() == -1) {
+                    frameAvailability++;
+                    mainMemoryFrameAvailability.add(i);
+                }
+            }
+            
+            System.out.println("Se usaron los siguientes marcos de página: ");
+            
+            fitsInMainMemory = (p.getPageNumber() <= frameAvailability);
+                        
+            if (!fitsInMainMemory) {
+                //swap usando FIFO
+                
+            }
+            //solo se carga en memoria
+                int frameNumber = 0;
+                int pageNumber = 0;
+                
+                for (int i = 0; i < frameAvailability; i++) {
+                    frameNumber = mainMemoryFrameAvailability.get(i);
+                    mainMemory[frameNumber].setProcessID(p.getId());
+                    mainMemory[frameNumber].setPageNumber(pageNumber);
+                    System.out.println(frameNumber + " ");
+                    pageNumber++;
+                }
+                
+                mainMemoryFrameAvailability.clear();
+                System.out.println();
+        }
+    }
+        
+    public boolean isLoaded (Process p) {
+        
+        int pageNum = p.getSize()/8;
+        if (p.getSize() % 8 != 0) {
+            pageNum++;
+        }
+        
+        p.setPageNumber(pageNum);
+        
+        boolean isLoaded = false;
+        
+        for (int i = 0; i < 256; i++) {
+            if (mainMemory[i].getProcessID() == p.getId() || 
+                    secondaryMemory[i].getProcessID() == p.getId()) {
+                return true;
+            }
+        }
+        
+        for (int i = 256; i < 512; i++) {
+            if (secondaryMemory[i].getProcessID() == p.getId()) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
